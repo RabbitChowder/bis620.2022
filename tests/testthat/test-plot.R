8427dfeab0f989757df425d549eee02193516963
hl896 <hang.li.hl896@yale.edu>
2022-11-23 04:05:43 -0500

    resolve issues with lintr

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -2,7 +2,7 @@ test_that(
   "The accel_plot() returns a ggplot object.",
   {
     data(ukb_accel)
-    p =  accel_plot(ukb_accel[1:100,])
+    p <-  accel_plot(ukb_accel[1:100, ])
     expect_true(inherits(p, "gg"))
   }
 )
@@ -19,7 +19,7 @@ test_that(
   "The accel_plot() is correct for time-series data.",
   {
     data(ukb_accel)
-    p = accel_plot(ukb_accel[1:100,])
+    p <- accel_plot(ukb_accel[1:100, ])
     vdiffr::expect_doppelganger("first-100-samples", p)
   }
 )
@@ -28,7 +28,7 @@ test_that(
   "The accel_plot() is correct for time-series data.",
   {
     data(ukb_accel)
-    s<-spectral_signature(ukb_accel[1:100, ], take_log = TRUE)
+    s <- spectral_signature(ukb_accel[1:100, ], take_log = TRUE)
     expect_true(inherits(s, "tbl_df"))
   }
 )
@@ -37,7 +37,7 @@ test_that(
   "The accel_plot() is correct for time-series data.",
   {
     data(ukb_accel)
-    s<-spectral_signature(ukb_accel[1:100, ], take_log = F)
+    s <- spectral_signature(ukb_accel[1:100, ], take_log = FALSE)
     p <- accel_plot(s)
     vdiffr::expect_doppelganger("first-100-samples-specsig", p)
   }
